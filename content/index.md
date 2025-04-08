94896532cb23d6e25dd1156384de34f1edd93745
Vojta Netrh <73762553+Nert53@users.noreply.github.com>
2025-04-08 20:08:27 +0200

    small changes

--- content/index.md
+++ content/index.md
@@ -1,6 +1,6 @@
 ---
-title: Welcome to Quartz
+title: Welcome to my Page
 ---
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+This page is currently in setup environment.
+
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Email: "vojtanetrh@gmail.com",
     },
   }),
 }
@@ -40,7 +39,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
